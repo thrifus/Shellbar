5cdf2ec0c61496770bbc73f34c936ff6fef1cd21
Patrick Hart <thrifus@gmail.com>
2015-11-18 14:58:04 -0600

    Initial Commit

---
+++ Shellbar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Shellbar
+//
+//  Created by Patrick Hart on 11/18/15.
+//  Copyright © 2015 thrifus. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
