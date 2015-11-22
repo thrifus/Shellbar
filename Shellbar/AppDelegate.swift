afa30ce7159095fdd07b78cab9ed17bc358529c5
Patrick Hart <thrifus@gmail.com>
2015-11-21 20:36:26 -0600

    Final commit of v1.0

--- Shellbar/AppDelegate.swift
+++ Shellbar/AppDelegate.swift
@@ -9,7 +9,7 @@
 
 import Foundation
 import Cocoa
-import WebKit
+import AppKit
 import SwiftShell
 
 @NSApplicationMain
@@ -17,60 +17,61 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var view: NSView!
-    @IBOutlet var aboutWebView: WebView!
     
-    var urlpath = NSBundle.mainBundle().pathForResource("about", ofType: "html")
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
     
-    func loadAddressURL() {
-        let requesturl = NSURL(string: urlpath!)
-        let request = NSURLRequest(URL: requesturl!)
-        aboutWebView.mainFrame.loadRequest(request)
-    }
-    
-    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
-    
-    // Notification Functions
-    func ShowNotify() -> Void {
-        let notification = NSUserNotification()
-        notification.title = "Showing hidden files"
-        notification.informativeText = "Finder will restart, and you'll probably notice it"
-        notification.soundName = NSUserNotificationDefaultSoundName
-        //notification.contentImage = NSImage(named: "StatusBarIcon")
-        NSUserNotificationCenter.defaultUserNotificationCenter().deliverNotification(notification)
-    }
-    func HideNotify() -> Void {
-        let notification = NSUserNotification()
-        notification.title = "Hiding hidden files"
-        notification.informativeText = "Finder will restart, and you'll probably notice it"
-        notification.soundName = NSUserNotificationDefaultSoundName
-        NSUserNotificationCenter.defaultUserNotificationCenter().deliverNotification(notification)
-    }
-    
-    // Show Hidden Files
+    // SHOW HIDDEN FILES
     func ShowFiles(sender: AnyObject) {
-        ShowNotify()
+        // NOTIFY
+        let SHFNotification = NSUserNotification()
+        SHFNotification.title = "Showing hidden files"
+        SHFNotification.informativeText = "Finder will restart, and you'll probably notice it"
+        SHFNotification.soundName = NSUserNotificationDefaultSoundName
+        NSUserNotificationCenter.defaultUserNotificationCenter().deliverNotification(SHFNotification)
+        // RUN
         run("defaults write com.apple.finder AppleShowAllFiles TRUE && killall Finder")
     }
-    // Hide Hidden Files
+    // HIDE HIDDEN FILES
     func HideFiles(sender: AnyObject) {
-        HideNotify()
+        // NOTIFY
+        let HHFNotification = NSUserNotification()
+        HHFNotification.title = "Hiding hidden files"
+        HHFNotification.informativeText = "Finder will restart, and you'll probably notice it"
+        HHFNotification.soundName = NSUserNotificationDefaultSoundName
+        NSUserNotificationCenter.defaultUserNotificationCenter().deliverNotification(HHFNotification)
+        // RUN
         run("defaults write com.apple.finder AppleShowAllFiles FALSE && killall Finder")
     }
-    // Clear Logs
+    // CLEAR LOGS
     func ClearLogs(sender: AnyObject) {
-        //ClearLogsNotify()
+        // RUN
         run("osascript -e 'do shell script \"sudo rm -fdrv /tmp/*; sudo rm -fdrv /var/tmp/*; sudo rm -fdrv /var/log/*; sudo rm -fdrv /private/tmp/*; sudo rm -fdrv /private/var/log/*\" with administrator privileges'")
     }
-    // Empty Trash
+    // EMPTY TRASH
     func EmptyTrash(sender: AnyObject) {
-        //EmptyTrashNotify()
+        // RUN
         run("osascript -e 'do shell script \"sudo rm -fdrv ~/.Trash/*\" with administrator privileges'")
     }
-    // About Shellbar
+    // ABOUT SHELLBAR
     func AboutShellbar(sender: AnyObject) {
-        self.window!.orderFront(self)
-        NSApplication.sharedApplication().activateIgnoringOtherApps(true)
-        loadAddressURL()
+        let AboutAlert: NSAlert = NSAlert()
+        AboutAlert.messageText = "Created by Patrick Hart\nhttp://thrifus.co/"
+        AboutAlert.informativeText = "If you find a bug in Shellbar, please, contact me. Make sure you use lots of details in your report, too.\nI can't help if you just complain."
+        AboutAlert.addButtonWithTitle("Close")
+        AboutAlert.addButtonWithTitle("Contact Me")
+        if AboutAlert.runModal() == NSAlertSecondButtonReturn {
+            let os = NSProcessInfo().operatingSystemVersion
+            let OSTYPE = String("OS X \(os.majorVersion).\(os.minorVersion).\(os.patchVersion)")
+            print(OSTYPE)
+            
+            let body = "NOTE: IF YOU ARE SUBMITTING ONE OR THE OTHER, JUST TYPE \"NONE\" IN THE ONE YOU DON'T WANT TO SUBMIT.\nYOU CAN DELETE THIS PART NOW.\n\nBUG REPORT:\n<REPLACE WITH THE BUG YOU FOUND>\n\n\nFEATURE REQUEST:\n<REPLACE WITH YOUR FEATURE REQUEST>"
+            let shareItems = [body] as NSArray
+            let service = NSSharingService(named: NSSharingServiceNameComposeEmail)
+            service?.recipients = ["admin@thrifus.co"]
+            let subject = "<CHANGE THIS TO YOUR OWN SUBJECT DEPENDING ON WHAT YOU ARE SUBMITTING>"
+            service?.subject = subject
+            service?.performWithItems(shareItems as [AnyObject])
+        }
     }
     
     // NOTE: aNotificaton
@@ -79,17 +80,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             button.image = NSImage(named: "StatusBarIcon")
         }
         
-        if let button = statusItem.button {
-        button.image = NSImage(named: "StatusBarIcon")
-        button.action = Selector("AboutShellbar:")
-        button.action = Selector("ShowFiles:")
-        button.action = Selector("HideFiles:")
-        button.action = Selector("ClearLogs:")
-        button.action = Selector("EmptyTrash:")
-        }
-        
         let menu = NSMenu()
-        // Menu Items
+        // MENU ITEMS
         menu.addItem(NSMenuItem(title: "About Shellbar", action: Selector("AboutShellbar:"), keyEquivalent: ""))
         menu.addItem(NSMenuItem.separatorItem())
         menu.addItem(NSMenuItem(title: "Show Hidden Files", action: Selector("ShowFiles:"), keyEquivalent: ""))
@@ -102,6 +94,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         statusItem.menu = menu
     }
-    
-    func applicationWillTerminate(aNotification: NSNotification) {}
+    // NOTE: aNotification
+    func applicationWillTerminate(notification: NSNotification) {}
 }
